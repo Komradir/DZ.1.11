79c6d60cc84aefe60bc3be78dc988a5cec8b6943
Komradir <b-d45@mail.ru>
2021-04-08 18:22:18 +0300

    comment

--- src/dima/baianov/Main5.java
+++ src/dima/baianov/Main5.java
@@ -1,43 +1,41 @@
 package dima.baianov;
-
 import java.util.Arrays;
 public class Main5 {
     public static void main(String args[]) {
-        int  [][] arrayTwoD  = {
-
-                {1,15,7,21,228,1488,322,228},
-                {9,77,69,55,2,4,6,0},
-                {22,33,44,55,66,77,88,11},
-                {34,52,11,56,78,99,88,12},
-                {67,55,32,3,16,64,1337,8},
-                {9,78,90,91,87,78,51,72},
-
-
+        int[][] arrayTwoD = {
+                {1, 15, 7, 21, 228, 1488, 322, 228},
+                {9, 77, 69, 55, 2, 4, 6, 0},
+                {22, 33, 44, 55, 66, 77, 88, 11},
+                {34, 52, 11, 56, 78, 99, 88, 12},
+                {67, 55, 32, 3, 16, 64, 1337, 8},
+                {9, 78, 90, 91, 87, 78, 51, 72},
         };
         System.out.println();
-        
-        int [][] mas;
 
-        for (int  [] arrayOneD : arrayTwoD) {
+
+        for (int[] arrayOneD : arrayTwoD) {
 
             Arrays.sort(arrayOneD);
+            System.out.println(arrayOneD);
 
-            }
-
-        mas [0][0] = arrayTwoD[0][0];
-        mas [0][1] = arrayTwoD[0][7];
-        mas [1][0] = arrayTwoD[1][0];
-        mas [1][1] = arrayTwoD[1][7];
-        mas [2][0] = arrayTwoD[2][0];
-        mas [2][1] = arrayTwoD[2][7];
-        mas [3][0] = arrayTwoD[3][0];
-        mas [3][1] = arrayTwoD[3][7];
-        mas [4][0] = arrayTwoD[4][0];
-        mas [4][1] = arrayTwoD[4][7];
-        mas [5][0] = arrayTwoD[5][0];
-        mas [5][1] = arrayTwoD[5][7];
-
-        System.out.println(mas);
         }
+        int p1 = arrayTwoD[0][0];
+        int a1 = arrayTwoD[0][7];
+        int s1 = arrayTwoD[1][0];
+        int d1 = arrayTwoD[1][7];
+        int l1 = arrayTwoD[4][7];
+        int z1 = arrayTwoD[5][0];
+        int x1 = arrayTwoD[5][7];
+
+
+        int[][] mas = {
+                {p1, a1},
+                {s1, d1},
+                {z1, x1},
+        };
+        System.out.println(mas);
+
     }
+}
+
 
